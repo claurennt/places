43dc7e6200a236dd9d70b7b16d5e3db6c3129d31
claurennt <c.benedetti1989@gmail.com>
2023-08-21 15:54:22 +0200

    initial commit

---
+++ db/connect.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import sanitizedConfig from '../config.js';
+
+mongoose
+  .connect(sanitizedConfig.MONGODB_CONNECTION_STRING)
+  .then(() => console.log('Connection to the database successful'))
+  .catch((err) => {
+    console.log('ERROR! Connection to the database failed', err);
+  });
+
+const db = mongoose.connection;
+
+db.on('error', (error: Error) => console.log(error.message));
+
+export default db;
---
+++ index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import sanitizedConfig from './config.js';
+import './db/connect.js';
+
+import { rootValue, schema } from './gql/index.js';
+
+const { PORT } = sanitizedConfig;
+
+const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
+
+console.log(`Running a GraphQL API server at http://localhost:${PORT}/graphql`);
