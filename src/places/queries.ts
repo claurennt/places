c98ec84912b35ee73ed8ff5b56d30797b44b403d
claurennt <c.benedetti1989@gmail.com>
2023-11-08 10:25:49 +0100

    feat: update places resolvers, add helpers

--- src/places/queries.ts
+++ src/places/queries.ts
@@ -1,1 +1,22 @@
-export const findAllPlaces = () => {};
+import { Place, IPlace } from '../db/index.js';
+import { checkDocumentExistence } from '../helpers/index.js';
+
+export const getPlace = async (args: {
+  _id: IPlace['_id'];
+}): Promise<IPlace | string | Error | null> => {
+  try {
+    const { _id } = args;
+
+    const foundPlace = checkDocumentExistence(_id, Place);
+    if (!foundPlace)
+      return `Place not found: a place with _id "${_id}" does not exist.`;
+
+    return foundPlace;
+  } catch (err) {
+    console.log(err);
+
+    return err;
+  }
+};
+
+// TODO : getPlaces by argument
